780254131292a84eb35abbd8aae2b9629f48c6f2
Adrian Pavel <adrian199909@gmail.com>
2022-04-21 20:39:39 +0200

    wordpress author user creation

--- Makefile
+++ Makefile
@@ -1,28 +1,32 @@
 NAME=inception
 
-DOCKER_COMPOSE_DIR=./srcs/
-NGINX=./srcs/nginx
-MARIADB=./srcs/mariadb
-WORDPRESS=./srcs/wordpress
+DOCKER_COMPOSE_FILE = ./srcs/docker-compose.yaml
+COMPOSE = docker-compose -f $(DOCKER_COMPOSE_FILE)
 
 all: $(NAME)
 
-$(NAME): create_volumes_folders up
+$(NAME): create_volumes_folders build up
+
+build:
+	$(COMPOSE) build
 
 up:
-	docker-compose up -df $(DOCKER_COMPOSE_DIR)
+	$(COMPOSE) up -d
 
 create_volumes_folders:
 	mkdir -p ~/data/mariadb
 	mkdir -p ~/data/wordpress
 
 down:
-	docker-compose down -f srcs/
+	$(COMPOSE) down
 
 clean:
 	docker container prune -f
 
+clean_volume:
+	docker volume rm $$(docker volume ls -q)
+
 fclean:
 	docker system prune -f
 
-re: 
+re:	build up
--- srcs/requirments/wordpress/conf/setup_wp.sh
+++ srcs/requirments/wordpress/conf/setup_wp.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-wp core download --path=/var/www/wordpress/ --allow-root
+wp core download --path=/var/www/wordpress/ --allow-root;
 wp config create --path=/var/www/wordpress/ --dbhost=mariadb --dbname=$WORDPRESS_DB_NAME --dbuser=$MYSQL_ADMIN_USERNAME --dbpass=$MYSQL_ADMIN_PASSWORD --allow-root;
-wp db create --allow-root;
+wp db create --path=/var/www/wordpress/ --allow-root;
 wp core install --path=/var/www/wordpress/ --url=localhost --title="Prueba" --admin_user=$WP_ADMIN_USER_NAME --admin_password=$WP_ADMIN_USER_PASS --admin_email=$WP_ADMIN_USER_EMAIL --allow-root;
-wp user create $WP_SECOND_USER_NAME $WP_SECOND_USER_EMAIL --user_pass=$WP_SECOND_USER_PASS --role=author
+wp user create --path=/var/www/wordpress/ $WP_AUTHOR_USER_NAME $WP_AUTHOR_USER_EMAIL --user_pass=$WP_AUTHOR_USER_PASS --role=author
 
 exec php-fpm7.3 --nodaemonize
